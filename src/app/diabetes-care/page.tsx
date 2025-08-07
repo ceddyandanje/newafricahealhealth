9bf7a9a8a65ea5d349353794f33f8baee79687ca
CEDRICK A. <ceddyandanje@gmail.com>
2025-08-07 13:48:39 +0000

    that not for respiratory page. iyts for diabetes page, udo the image cha

--- src/app/diabetes-care/page.tsx
+++ src/app/diabetes-care/page.tsx
@@ -8,7 +8,7 @@ export default function DiabetesCarePage() {
       <div className="bg-background">
         <section className="relative h-[50vh] min-h-[400px] flex items-center justify-center text-center text-white">
             <Image
-            src="https://placehold.co/1920x800.png"
+            src="https://images.unsplash.com/photo-1685485276223-0bb0226dcca8?q=80&w=756&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Person checking blood sugar level with a glucometer"
             fill
             className="object-cover"
