81132e6c4a82708e8deb107e3efdfe3795b7628a
CEDRICK A. <ceddyandanje@gmail.com>
2025-08-07 13:19:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/emergency-services/page.tsx
+++ src/app/emergency-services/page.tsx
@@ -40,7 +40,7 @@ export default function EmergencyServicesPage() {
           </div>
           <div className="relative h-80 rounded-2xl overflow-hidden glassmorphic p-2">
               <Image
-                  src="https://images.unsplash.com/photo-1588543385566-413e13a51a24?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://images.unsplash.com/photo-1586368635215-f13c34d616e5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="Helicopter ambulance flying over a landscape"
                   fill
                   className="object-cover rounded-xl"
