4256c8a0fbb437659b39b9c6b34ef3868c2b956c
CEDRICK A. <ceddyandanje@gmail.com>
2025-08-19 11:48:16 +0000

    FOR THE CARDIOVASCULAR PAGE, THE MAIN HERO IMAGE, USE THIS :https://imag

--- src/app/cardiovascular/page.tsx
+++ src/app/cardiovascular/page.tsx
@@ -8,7 +8,7 @@ export default function CardiovascularPage() {
       <div className="bg-background">
         <section className="relative h-[50vh] min-h-[400px] flex items-center justify-center text-center text-white">
             <Image
-            src="https://images.unsplash.com/photo-1506126613408-4e0521b2591e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            src="https://images.unsplash.com/photo-1690787628851-d36e285c29b0?q=80&w=851&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Illustration of a healthy heart with EKG lines"
             fill
             className="object-cover"
