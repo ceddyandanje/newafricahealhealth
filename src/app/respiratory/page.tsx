5d047f512ba088c0040756edf8f501282171e879
CEDRICK A. <ceddyandanje@gmail.com>
2025-08-19 12:49:32 +0000

    for the respiratory page use this as the main hero banner image :https:/

--- src/app/respiratory/page.tsx
+++ src/app/respiratory/page.tsx
@@ -8,7 +8,7 @@ export default function RespiratoryPage() {
       <div className="bg-background">
         <section className="relative h-[50vh] min-h-[400px] flex items-center justify-center text-center text-white">
             <Image
-            src="https://images.unsplash.com/photo-1604015343469-536767b6d6f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            src="https://images.unsplash.com/photo-1637930030080-4ec4b95914c0?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Person breathing fresh air in a mountain landscape"
             fill
             className="object-cover"
